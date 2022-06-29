227a2fae0c16acf65a59028c78dc8feb91760f22
Joshua Morony <joshua.morony@gmail.com>
2022-06-29 10:38:15 +0930

    chore: add angular/fire

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,14 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'joshmorony-yt-test',
+    appId: '1:643155649449:web:a2162b1dd0a13b0a1d1ab8',
+    storageBucket: 'joshmorony-yt-test.appspot.com',
+    apiKey: 'AIzaSyBuxLrGusl1lQK5PJS_i46Gm29ZQvDzA7k',
+    authDomain: 'joshmorony-yt-test.firebaseapp.com',
+    messagingSenderId: '643155649449',
+  },
   production: false
 };
 
