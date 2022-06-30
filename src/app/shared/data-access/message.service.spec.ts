cd9de666f99cff23d15b12c7b82d8497b57a357b
Joshua Morony <joshua.morony@gmail.com>
2022-06-30 13:34:18 +0930

    add test for getMessages()

--- src/app/shared/data-access/message.service.spec.ts
+++ src/app/shared/data-access/message.service.spec.ts
@@ -1,15 +1,54 @@
 import { TestBed } from '@angular/core/testing';
 import { MessageService } from './message.service';
+import * as AngularFireFirestore from '@angular/fire/firestore';
+import { subscribeSpyTo } from '@hirez_io/observer-spy';
+import { of } from 'rxjs';
+
+jest.mock('@angular/fire/firestore');
 
 describe('MessageService', () => {
   let service: MessageService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [AngularFireFirestore.Firestore],
+    });
     service = TestBed.inject(MessageService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  describe('getMessages()', () => {
+    it('should return a stream of messages from the messages collection with ids', () => {
+      const mockCollectionRef = jest.fn();
+      const mockDocumentData = jest.fn();
+
+      jest
+        .spyOn(AngularFireFirestore, 'collection')
+        .mockReturnValue(of(mockCollectionRef) as any);
+
+      jest
+        .spyOn(AngularFireFirestore, 'collectionData')
+        .mockReturnValue(of(mockDocumentData) as any);
+
+      const observerSpy = subscribeSpyTo(service.getMessages());
+      const options = {
+        idField: 'id',
+      };
+
+      expect(AngularFireFirestore.collection).toHaveBeenCalledWith(
+        {},
+        'messages'
+      );
+
+      expect(AngularFireFirestore.collectionData).toHaveBeenCalledWith(
+        mockCollectionRef,
+        options
+      );
+
+      expect(observerSpy.getLastValue()).toEqual(mockDocumentData);
+    });
   });
 });
--- src/app/shared/data-access/message.service.ts
+++ src/app/shared/data-access/message.service.ts
@@ -1,6 +1,11 @@
 import { Injectable } from '@angular/core';
+import { of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
-export class MessageService {}
+export class MessageService {
+  getMessages() {
+    return of({});
+  }
+}
