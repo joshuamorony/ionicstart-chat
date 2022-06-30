2e63bbaba3c1d0a1c17410cd12ff458cc0cd6bfc
Joshua Morony <joshua.morony@gmail.com>
2022-06-30 10:49:27 +0930

    convert home component to sfc

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,59 @@
-import { Component } from '@angular/core';
+import { NgModule, ChangeDetectionStrategy, Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { IonicModule } from '@ionic/angular';
+import { FormsModule } from '@angular/forms';
+
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  template: `
+    <ion-header [translucent]="true">
+      <ion-toolbar>
+        <ion-title> Blank </ion-title>
+      </ion-toolbar>
+    </ion-header>
+
+    <ion-content [fullscreen]="true">
+      <ion-header collapse="condense">
+        <ion-toolbar>
+          <ion-title size="large">Blank</ion-title>
+        </ion-toolbar>
+      </ion-header>
+
+      <div id="container">
+        <strong>Ready to create an app?</strong>
+        <p>
+          Start with Ionic
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://ionicframework.com/docs/components"
+            >UI Components</a
+          >
+        </p>
+      </div>
+    </ion-content>
+  `,
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HomePage {
-
   constructor() {}
-
 }
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    RouterModule.forChild([
+      {
+        path: '',
+        component: HomePage,
+      },
+    ]),
+  ],
+  declarations: [HomePage],
+  exports: [RouterModule],
+})
+export class HomePageModule {}
