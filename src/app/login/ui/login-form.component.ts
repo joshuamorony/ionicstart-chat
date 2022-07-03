86a395d6937fdc498921bc00def4856cb627a624
Joshua Morony <joshua.morony@gmail.com>
2022-07-04 08:48:18 +0930

    outline for login form template

--- src/app/login/ui/login-form.component.ts
+++ src/app/login/ui/login-form.component.ts
@@ -4,7 +4,19 @@ import { IonicModule } from '@ionic/angular';
 
 @Component({
   selector: 'app-login-form',
-  template: ``,
+  template: `
+    <form>
+      <ion-item>
+        <ion-label>email</ion-label>
+        <ion-input data-test="login-email-field" type="email"></ion-input>
+      </ion-item>
+      <ion-item>
+        <ion-label>password</ion-label>
+        <ion-input data-test="login-password-field" type="password"></ion-input>
+      </ion-item>
+      <ion-button data-test="login-button" type="submit">Login</ion-button>
+    </form>
+  `,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class LoginFormComponent {}
