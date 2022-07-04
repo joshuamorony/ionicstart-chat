5e788eef4047a587e4fff1054aff80c234a9d09f
Joshua Morony <joshua.morony@gmail.com>
2022-07-04 15:29:28 +0930

    implement createStatus stream

--- src/app/create/create-modal.component.spec.ts
+++ src/app/create/create-modal.component.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
+import { subscribeSpyTo } from '@hirez_io/observer-spy';
 import { IonicModule } from '@ionic/angular';
 import { AuthService } from '../shared/data-access/auth.service';
 import { CreateModalComponent } from './create-modal.component';
@@ -8,6 +9,11 @@ import { MockCreateFormComponent } from './ui/create-form.component.spec';
 describe('CreateModalComponent', () => {
   let component: CreateModalComponent;
   let fixture: ComponentFixture<CreateModalComponent>;
+
+  const testCredentials = {
+    email: 'test@test.com',
+    password: 'abc123',
+  };
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
@@ -33,14 +39,52 @@ describe('CreateModalComponent', () => {
     expect(component).toBeTruthy();
   });
 
+  it('should dismiss when close button is clicked', () => {});
+
+  it('should dismiss after successful account creation', () => {});
+
+  it('should display an error if account creation fails', () => {});
+
+  describe('createStatus$', () => {
+    it('should be pending initially', () => {
+      const observerSpy = subscribeSpyTo(component.createStatus$);
+      expect(observerSpy.getLastValue()).toEqual('pending');
+    });
+
+    it('should be creating when a login is in process', () => {
+      const observerSpy = subscribeSpyTo(component.createStatus$);
+
+      component.createAccount(testCredentials);
+
+      expect(observerSpy.getLastValue()).toEqual('creating');
+    });
+
+    it('should be success when a login has succeeded', async () => {
+      const observerSpy = subscribeSpyTo(component.createStatus$);
+
+      const authService = fixture.debugElement.injector.get(AuthService);
+      jest.spyOn(authService, 'createAccount').mockResolvedValue({} as any);
+
+      await component.createAccount(testCredentials);
+
+      expect(observerSpy.getLastValue()).toEqual('success');
+    });
+
+    it('should be error when a login has failed', async () => {
+      const observerSpy = subscribeSpyTo(component.createStatus$);
+
+      const authService = fixture.debugElement.injector.get(AuthService);
+      jest.spyOn(authService, 'createAccount').mockRejectedValue({} as any);
+
+      await component.createAccount(testCredentials);
+
+      expect(observerSpy.getLastValue()).toEqual('error');
+    });
+  });
+
   describe('createAccount()', () => {
     it('should pass data from create-form to createAccount method of auth service', () => {
       const authService = fixture.debugElement.injector.get(AuthService);
-
-      const testCredentials = {
-        email: 'test@test.com',
-        password: 'abcd1234',
-      };
 
       const createForm = fixture.debugElement.query(By.css('app-create-form'));
 
--- src/app/create/create-modal.component.ts
+++ src/app/create/create-modal.component.ts
@@ -1,6 +1,8 @@
 import { ChangeDetectionStrategy, Component, NgModule } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
+import { BehaviorSubject } from 'rxjs';
 import { AuthService } from '../shared/data-access/auth.service';
+import { Credentials } from '../shared/interfaces/credentials';
 import { CreateFormComponentModule } from './ui/create-form.component';
 
 @Component({
@@ -8,15 +10,28 @@ import { CreateFormComponentModule } from './ui/create-form.component';
   template: `
     <ion-header></ion-header>
     <ion-content>
-      <app-create-form
-        (create)="authService.createAccount($event)"
-      ></app-create-form>
+      <app-create-form (create)="createAccount($event)"></app-create-form>
     </ion-content>
   `,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CreateModalComponent {
+  createStatus$ = new BehaviorSubject<
+    'pending' | 'creating' | 'success' | 'error'
+  >('pending');
+
   constructor(protected authService: AuthService) {}
+
+  async createAccount(credentials: Credentials) {
+    this.createStatus$.next('creating');
+
+    try {
+      await this.authService.createAccount(credentials);
+      this.createStatus$.next('success');
+    } catch (err) {
+      this.createStatus$.next('error');
+    }
+  }
 }
 
 @NgModule({
